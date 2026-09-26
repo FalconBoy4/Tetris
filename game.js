9bcdd277abca1a2c770c84f01ba25cf26fea5a20
JacobGGray <gray.jg@oundleschool.org.uk>
2026-09-26 10:52:25 +0100

    added animations

--- game.js
+++ game.js
@@ -1,9 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const next_canvas = document.getElementById("nextCanvas");
-const ctx = canvas.getContext("2d");
+const grid_ctx = canvas.getContext("2d");
 const next_ctx = next_canvas.getContext("2d");
-
-const speed = 1
 
 const shapes = [
     // I
@@ -50,31 +48,47 @@ const colours = [
     "#00FF00",
     "#FF0000",
     "#FFA500",
-    "#0000FF"
+    "#0000FF",
+    "#ffffff"
 ]
 
 class Display {
     constructor(canvas_ID) {
         this.canvas = document.getElementById(canvas_ID);
         this.ctx = this.canvas.getContext("2d");
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
         this.grid = new Grid(Math.floor(this.canvas.width / tile_size), Math.floor(this.canvas.height / tile_size))
+        this.animator = new AnimationManager()
+        this.paused = false
     }
 
-    update() {
-        this.grid.update()
+    update(dt) {
+        this.animator.update(dt)
+
+        if (this.paused) return;
+
+        this.grid.update(dt)
     }
 
     draw() {
-        this.grid.draw()
+        this.clear()
+        this.grid.draw(this.ctx)
+        this.animator.draw(ctx)
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
     }
 }
 
 class Grid {
-    constructor(width, height, current_ctx) {
-        this.current_ctx = (current_ctx) ? current_ctx : ctx;
+    constructor(width, height, ctx) {
+        this.ctx = ctx
         this.width = width;
         this.height = height;
         this.matrix = this.new_matrix();
+        this.anims = [];
     }
 
     fits(x, y) {
@@ -98,6 +112,10 @@ class Grid {
     }
 
     clear_row(row) {
+        animations.anim_row(row)
+    }
+
+    _clear_row(row) {
         this.matrix.splice(row, 1);
         this.matrix.unshift(Array(this.width).fill(0));
     }
@@ -113,12 +131,76 @@ class Grid {
         return cleared
     }
 
-    draw() {
+    update(dt) {
+        score(this.check())
+    }
+
+    draw(ctx) {
         this.matrix.forEach((row, y) => {
             row.forEach((element, x) => {
-                draw_square(x, y, element, this.current_ctx);
+                draw_square_fill(ctx, x, y, element);
             });
         });
+    }
+
+    get is_clear() {
+        return Math.max(...this.matrix) === 0;
+    }
+}
+
+class AnimationManager {
+    constructor() {
+        this.anims = []
+    }
+
+    update(dt) {
+        this.anims = this.anims.filter(anim => {
+            anim.elapsed += dt;
+            const done = anim.elapsed >= anim.duration
+            if (done) anim.on_end(...anim.params);
+            return !done;
+        })
+
+        paused = this.anims.length !== 0;
+    }
+
+    draw(ctx) {
+        this.anims.forEach(anim => {
+            const progress = Math.min(1, anim.elapsed / anim.duration);
+            anim.draw(ctx, progress, ...anim.params);
+        })
+    }
+
+    add(draw, on_end = () => {}, duration, params = []) {
+        paused = true
+        this.anims.push({
+            draw,
+            on_end: on_end.bind(grid),
+            duration,
+            elapsed: 0,
+            params
+        });
+    }
+
+    anim_row(row) {
+        this.add(this.anim_row_draw, grid._clear_row, 0.3, [row])
+    }
+
+    anim_row_draw(ctx, progress, row) {
+        if (progress < 0.5) {
+            const fill_progress = progress / 0.5
+            const fill_squares = Math.ceil(grid.width * fill_progress)
+            for (let x = 0; x < fill_squares; x++) {
+                draw_square_fill(ctx, x, row, 8)
+            }
+        } else {
+            const empty_progress = (progress - 0.5) / 0.5
+            const empty_squares = Math.floor(grid.width * empty_progress)
+
+            for (let x = empty_squares; x < grid.width; x++) {
+                draw_square_fill(ctx, x, row, 8)
+            }
+        }
     }
 }
 
@@ -130,12 +212,10 @@ class Block {
         this.y = 0;
         this.width = this.shape_width * tile_size;
         this.height = this.shape_height * tile_size;
-        this.move_pause = FPS
-        this.move_timers = {
+        this.move_progress = {
             "y": 0,
             "x": 0
         };
-        this.speed = level + 1
     }
 
     move(x = 0, y = 0) {
@@ -146,7 +226,6 @@ class Block {
         this.x += x;
         valid = this.check_move(this.shape);
         if (valid && x != 0) {
-            this.move_timers["x"] = 0;
             moved = true;
         } else {
             this.x -= x;
@@ -156,7 +235,6 @@ class Block {
         this.y += y;
         valid = this.check_move(this.shape);
         if (valid && y != 0) {
-            this.move_timers["y"] = 0;
             moved = true;
         } else {
             this.y -= y;
@@ -165,44 +243,44 @@ class Block {
         return moved
     }
 
-    draw() {
+    draw(ctx) {
         this.shape.forEach((row, y) => {
             row.forEach((element, x) => {
                 if (element != 0) {
-                    draw_square(this.x + x, this.y + y, element);
+                    draw_square_outline(ctx, this.x + x, this.drop_y + y, element);
+                    draw_square_fill(ctx, this.x + x, this.y + y, element);
                 };
             });
         });
     }
 
-    update() {
-        this.move_timers["y"] += (keys["ArrowDown"] ? 30 : 1) * 0.5 * this.speed
-
-        if (this.move_timers["y"] >= this.move_pause) {
+    update(dt) {
+        this.move_progress.y += dt / (keys["ArrowDown"] ? intervals.min : intervals.fall);
+        if (this.move_progress.y >= 1) {
+            this.move_progress.y = 0;
             if (!this.move(0, 1)) {
                 this.kill();
                 return;
             };
         };
 
-        if (this.move_timers["x"] >= this.move_pause) {
+        this.move_progress.x += dt / intervals.side;
+        if (this.move_progress.x >= 1) {
             if (keys["ArrowRight"]) {
-                this.move(1, 0);
+                if (this.move(1, 0)) this.move_progress.x = 0;
             };
 
             if (keys["ArrowLeft"]) {
-                this.move(-1, 0);
+                if (this.move(-1, 0)) this.move_progress.x = 0;
             };
-        } else {
-            this.move_timers["x"] += 10;
         };
 
         if (keys_pressed["ArrowUp"]) this.rotate();
 
         if (keys_pressed[" "]) {
-            this.drop()
-            this.kill()
-            return
+            this.drop();
+            this.kill();
+            return;
         };
     }
 
@@ -218,7 +296,7 @@ class Block {
     }
 
     drop() {
-        while (this.move(0, 1));
+        this.y = this.drop_y
     }
 
     check_move(shape) {
@@ -252,20 +330,26 @@ class Block {
     get shape_height() {
         return this.shape.length
     }
+
+    get drop_y() {
+        const old_y = this.y
+        while (this.move(0, 1, false));
+        const new_y = this.y
+        this.y = old_y
+        return new_y
+    }
 }
 
-let score = 0;
+let current_score = 0;
+let last_cleared = 0;
 let lines = 0;
 let level = 1;
 
-const FPS = 60;
-setInterval(() => {
-    update();
-    draw();
-}, 1000 / FPS);
+let gameover = false
 
+const animations = new AnimationManager()
 const tile_size = 32
-const grid = new Grid(Math.floor(canvas.width / tile_size), Math.floor(canvas.height / tile_size))
+const grid = new Grid(Math.floor(canvas.width / tile_size), Math.floor(canvas.height / tile_size), grid_ctx)
 const next_grid = new Grid(Math.floor(next_canvas.width / tile_size), Math.floor(next_canvas.height / tile_size), next_ctx)
 let piece = new Block(random_shape())
 let next_shape = random_shape()
@@ -279,54 +363,36 @@ function new_piece() {
     next_shape = random_shape()
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    next_ctx.clearRect(0, 0, next_canvas.width, next_canvas.height);
-    grid.draw();
-    piece.draw();
-    next_grid.draw();
-    draw_next_shape();
-}
-
-function update() {
-    if (!gameover) {
-        piece.update()
-        cleared = grid.check()
-        lines += cleared
-        score += calc_score(cleared)
-        if (lines >= 10 * level) level += 1;
-        if (piece.dead) {
-            new_piece();
-            if (!piece.check_move(piece.shape)) gameover = true;
-        };
-        document.getElementById("level").innerText = level;
-        document.getElementById("score").innerText = score;
-        document.getElementById("lines").innerText = lines;
-        keys_pressed = {}
-    }
-}
-
-function draw_next_shape() {
+function draw_next_shape(ctx) {
     next_shape.forEach((row, y) => {
         row.forEach((element, x) => {
-            draw_square(x, y, element, next_ctx);
+            draw_square_fill(ctx, x, y, element, next_ctx);
         });
     });
 }
 
 const img = new Image();
 img.src = "block.png";
-let gameover = false;
 
-function draw_square(x, y, colour_index, current_ctx = null) {
-    if (!current_ctx) current_ctx = ctx
-    current_ctx.globalCompositeOperation = 'source-over';
+function draw_square_fill(ctx, x, y, colour_index) {
+    ctx.globalCompositeOperation = 'source-over';
     x *= tile_size
     y *= tile_size
-    current_ctx.drawImage(img, x, y)
-    current_ctx.fillStyle = colours[colour_index];
-    current_ctx.globalCompositeOperation = 'multiply';
-    current_ctx.fillRect(x, y, tile_size, tile_size)
+    ctx.drawImage(img, x, y)
+    ctx.fillStyle = colours[colour_index];
+    ctx.globalCompositeOperation = 'multiply';
+    ctx.fillRect(x, y, tile_size, tile_size)
+}
+
+function draw_square_outline(ctx, x, y, colour_index) {
+    ctx.globalCompositeOperation = 'source-over';
+    thickness = Math.floor(tile_size / 16)
+    x = x * tile_size + thickness
+    y = y * tile_size + thickness
+    size = tile_size - thickness * 2
+    ctx.strokeStyle = colours[colour_index];
+    ctx.strokeRect(x, y, size, size)
+
 }
 
 let keys = {};
@@ -343,7 +409,7 @@ document.addEventListener("keyup", (e) => {
     locked[e.key] = false
 });
 
-scoring = [
+const score_dict = [
     0,
     40,
     100,
@@ -352,11 +418,76 @@ scoring = [
 ]
 
 function calc_score(cleared) {
-    return scoring[cleared] * level
+    return score_dict[cleared] * level
+}
+
+function score(cleared) {
+    if (cleared === 0) return;
+
+    const added = score_dict[cleared] * level;
+    if (last_cleared === cleared === 4) added *= 2;
+    current_score += added
 }
 
 function zeros(rows, cols) {
     return (Array.from({length: rows}, () =>
                 Array(cols).fill(0)
             ));
+}
+
+let last_time = performance.now()
+let paused = false
+
+function game_loop(now) {
+    if (gameover) return;
+
+    const dt = (now - last_time) / 1000;
+    last_time = now;
+
+    update(dt);
+    draw();
+
+    requestAnimationFrame(game_loop);
+}
+
+// Start
+requestAnimationFrame(game_loop)
+
+function draw() {
+    grid_ctx.clearRect(0, 0, canvas.width, canvas.height);
+    next_ctx.clearRect(0, 0, next_canvas.width, next_canvas.height);
+    grid.draw(grid_ctx);
+    piece.draw(grid_ctx);
+    next_grid.draw(next_ctx);
+    draw_next_shape(next_ctx);
+    animations.draw(grid_ctx)
+}
+
+function update(dt) {
+    animations.update(dt)
+
+    if (!paused) {
+        piece.update(dt)
+        cleared = grid.check()
+        lines += cleared
+        score += calc_score(cleared)
+        if (lines >= 10 * level) level += 1;
+        if (piece.dead) {
+            new_piece();
+            if (!piece.check_move(piece.shape)) gameover = true;
+        };
+        document.getElementById("level").innerText = level;
+        document.getElementById("score").innerText = current_score;
+        document.getElementById("lines").innerText = lines;
+    };
+
+    keys_pressed = {};
+}
+
+const intervals = {
+    "min": 0.05,
+    "side": 0.1,
+    get fall() {
+        return Math.max(intervals.min, Math.pow(0.8, level - 1))
+    }
 }
